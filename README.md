3b2ac6c5098f138e27d7185999e4e862e66ca7cd
Eddie Cuellar <heriberto.cuellar1329@gmail.com>
2020-08-21 00:17:30 -0500

    Completed the Rock Paper Scissors App

---
+++ README.md
@@ -0,0 +1,16 @@
+# Robot Uprising Rock Paper Scissors App
+A simple and fun Rock Paper Scissors App with a hint of dystopia written entirely in JavaScript, HTML and CSS
+
+Code Located [Here](https://github.com/hcuellar-coder/Rock-Paper-Scissors-Game)
+
+Run Code (Sans Images) [Here](https://codepen.io/hcuellar-coder/pen/yLOVMGV)
+
+![Todo App](./images/Rock-Paper-Scissors.png)
+
+## Summary
+Have you ever had to battle against a machine with humanity on the line? Our soon to be robotic enemies would want you to run away, but not this time. You're getting your reps in using this Rock Paper Scissors App. Which, in our isolated dystopian world, is the only training method you need in defeating the robot overlords! 
+
+I quite enjoyed the challenge of creating this simple Rock Paper Scissors App. I was able to utilize what I've been learning with keeping data in the local storage. As well as tying in what I've learned pertaining to the relationship between JavaScript, HTML and CSS.
+
+## Author
+Heriberto Cuellar – Full Stack Software Developer - [LinkedIn](linkedin.com/in/heriberto-c-5aa11952)
--- index.js
+++ index.js
@@ -1,10 +1,47 @@
 document.getElementById('rock').addEventListener('click', playGame);
 document.getElementById('paper').addEventListener('click', playGame);
 document.getElementById('scissors').addEventListener('click', playGame);
+document.getElementById('resetButton').addEventListener('click', clearGame);
 document.getElementById('results').style.visibility = 'hidden';
 
+let gameCount = 0;
+let manWins = 0;
+let machineWins = 0;
+
+if (window.localStorage.getItem('gameCount')) {
+    getCounts();
+} else {
+    zeroCounts();
+}
+
+function clearGame() {
+    window.localStorage.removeItem('gameCount');
+    window.localStorage.removeItem('manWins');
+    window.localStorage.removeItem('machineWins');
+    zeroCounts();
+    getCounts();
+    document.getElementById('results').style.visibility = 'hidden';
+    document.getElementById('resultHeader').style.visibility = 'hidden';
+}
+
+function getCounts() {
+    gameCount = window.localStorage.getItem('gameCount');
+    manWins = window.localStorage.getItem('manWins');
+    machineWins = window.localStorage.getItem('machineWins');
+    document.getElementById('manScore').innerHTML = 'Man : ' + `${manWins}`;
+    document.getElementById('machineScore').innerHTML = `${machineWins}` + ' : Machine';
+}
+
+function zeroCounts() {
+    window.localStorage.setItem('gameCount', 0);
+    window.localStorage.setItem('manWins', 0);
+    window.localStorage.setItem('machineWins', 0);
+}
+
 function playGame(e) {
-    document.getElementById('resultHeader').innerHTML = '';
+    gameCount++;
+    window.localStorage.setItem('gameCount', gameCount);
+    document.getElementsByClassName('resultHeader').innerHTML = '';
     computeWinner(e.target.id, machineOption());
 }
 
@@ -30,20 +67,32 @@ function computeWinner(man, machine) {
 }
 
 function displayResults(manOption, machineOption, result) {
-    let resultText;
-
+    let resultHeader;
     document.getElementById('man').innerHTML = document.getElementById(manOption).outerHTML;
     document.getElementById('machine').innerHTML = document.getElementById(machineOption).outerHTML;
+    document.getElementById('resultLabelMan').innerHTML = 'Man chooses ' + `${manOption}`;
+    document.getElementById('resultLabelMachine').innerHTML = 'Machine chooses ' + `${machineOption}`;
 
     if (result == 1) {
-        resultText = 'Victory to Man!'
+        resultHeader = 'Victory to Man!'
+        manWins++;
+        window.localStorage.setItem('manWins', manWins);
+        document.getElementById('manScore').innerHTML = 'Man : ' + `${manWins}`;
+        document.getElementById('resultHeader').className = 'manWinner';
     } else if (result == 2) {
-        resultText = 'Stalemate!';
+        resultHeader = 'Stalemate!';
+        document.getElementById('resultHeader').className = 'stalemate';
     } else {
-        resultText = 'The Machines have Won!'
+        resultHeader = 'The Machines have Won!'
+        machineWins++;
+        window.localStorage.setItem('machineWins', machineWins);
+        document.getElementById('machineScore').innerHTML = `${machineWins}` + ' : Machine';
+        document.getElementById('resultHeader').className = 'machineWinner';
     }
-
-    document.getElementById('resultHeader').innerHTML = resultText;
-
+    document.getElementById('resultHeader').innerHTML = resultHeader;
     document.getElementById('results').style.visibility = 'visible';
+    document.getElementById('resultHeader').style.visibility = 'visible';
+
+
+
 }
