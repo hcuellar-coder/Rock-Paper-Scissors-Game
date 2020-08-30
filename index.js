fabc6f29c590f3ce5b73d038ad83be422344bcb1
Eddie Cuellar <heriberto.cuellar1329@gmail.com>
2020-08-30 17:56:36 -0500

    Re-factored code

--- index.js
+++ index.js
@@ -1,51 +1,56 @@
 document.getElementById('rock').addEventListener('click', playGame);
 document.getElementById('paper').addEventListener('click', playGame);
 document.getElementById('scissors').addEventListener('click', playGame);
-document.getElementById('resetButton').addEventListener('click', clearGame);
+document.getElementById('reset-button').addEventListener('click', clearGame);
 document.getElementById('results').style.visibility = 'hidden';
 
-let gameCount = 0;
-let manWins = 0;
-let machineWins = 0;
+let gameCount = window.localStorage.getItem('gameCount') || 0;
+let manWins = window.localStorage.getItem('manWins') || 0;
+let machineWins = window.localStorage.getItem('machineWins') || 0;
 
-if (window.localStorage.getItem('gameCount')) {
-    getCounts();
-} else {
-    zeroCounts();
-}
-
-function clearGame() {
-    window.localStorage.removeItem('gameCount');
-    window.localStorage.removeItem('manWins');
-    window.localStorage.removeItem('machineWins');
-    zeroCounts();
-    getCounts();
-    document.getElementById('results').style.visibility = 'hidden';
-    document.getElementById('resultHeader').style.visibility = 'hidden';
-}
-
-function getCounts() {
-    gameCount = window.localStorage.getItem('gameCount');
-    manWins = window.localStorage.getItem('manWins');
-    machineWins = window.localStorage.getItem('machineWins');
-    document.getElementById('manScore').innerHTML = 'Man : ' + `${manWins}`;
-    document.getElementById('machineScore').innerHTML = `${machineWins}` + ' : Machine';
-}
-
-function zeroCounts() {
-    window.localStorage.setItem('gameCount', 0);
-    window.localStorage.setItem('manWins', 0);
-    window.localStorage.setItem('machineWins', 0);
-}
+displayScores();
 
 function playGame(e) {
     gameCount++;
+
+    document.getElementsByClassName('result-header').innerHTML = '';
+
+    const manChoice = e.target.id;
+    const machineChoice = getMachineChoice();
+
+    const gameResult = getGameResult(manChoice, machineChoice);
+
+    if (gameResult === 'W') manWins++;
+    if (gameResult === 'L') machineWins++;
+
+    displayResults(manChoice, machineChoice, gameResult);
+
     window.localStorage.setItem('gameCount', gameCount);
-    document.getElementsByClassName('resultHeader').innerHTML = '';
-    computeWinner(e.target.id, machineOption());
+    window.localStorage.setItem('manWins', manWins);
+    window.localStorage.setItem('machineWins', machineWins);
 }
 
-function machineOption() {
+function clearGame() {
+    gameCount = 0;
+    manWins = 0;
+    machineWins = 0;
+
+    window.localStorage.setItem('gameCount', gameCount);
+    window.localStorage.setItem('manWins', manWins);
+    window.localStorage.setItem('machineWins', machineWins);
+
+    document.getElementById('results').style.visibility = 'hidden';
+    document.getElementById('result-header').style.visibility = 'hidden';
+
+    displayScores();
+}
+
+function displayScores() {
+    document.getElementById('man-score').innerHTML = 'Man : ' + `${manWins}`;
+    document.getElementById('machine-score').innerHTML = `${machineWins}` + ' : Machine';
+}
+
+function getMachineChoice() {
     let randomOption = Math.floor(Math.random() * 3) + 1;
     if (randomOption == 1)
         return 'rock';
@@ -55,44 +60,38 @@ function machineOption() {
         return 'scissors';
 }
 
-function computeWinner(man, machine) {
+function getGameResult(man, machine) {
     if ((man == 'rock' && machine == 'scissors')
         || (man == 'paper' && machine == 'rock')
         || (man == 'scissors' && machine == 'paper')) {
-        displayResults(man, machine, 1);
+        return 'W';
     } else if (man === machine) {
-        displayResults(man, machine, 2);
+        return 'T';
     } else
-        displayResults(man, machine, 3);
+        return 'L';
 }
 
 function displayResults(manOption, machineOption, result) {
-    let resultHeader;
     document.getElementById('man').innerHTML = document.getElementById(manOption).outerHTML;
     document.getElementById('machine').innerHTML = document.getElementById(machineOption).outerHTML;
-    document.getElementById('resultLabelMan').innerHTML = 'Man chooses ' + `${manOption}`;
-    document.getElementById('resultLabelMachine').innerHTML = 'Machine chooses ' + `${machineOption}`;
 
-    if (result == 1) {
+    document.getElementById('result-label-man').innerHTML = 'Man chooses ' + `${manOption}`;
+    document.getElementById('result-label-machine').innerHTML = 'Machine chooses ' + `${machineOption}`;
+
+    let resultHeader;
+    if (result == 'W') {
         resultHeader = 'Victory to Man!'
-        manWins++;
-        window.localStorage.setItem('manWins', manWins);
-        document.getElementById('manScore').innerHTML = 'Man : ' + `${manWins}`;
-        document.getElementById('resultHeader').className = 'manWinner';
-    } else if (result == 2) {
+        document.getElementById('result-header').className = 'manWinner';
+    } else if (result == 'T') {
         resultHeader = 'Stalemate!';
-        document.getElementById('resultHeader').className = 'stalemate';
+        document.getElementById('result-header').className = 'stalemate';
     } else {
         resultHeader = 'The Machines have Won!'
-        machineWins++;
-        window.localStorage.setItem('machineWins', machineWins);
-        document.getElementById('machineScore').innerHTML = `${machineWins}` + ' : Machine';
-        document.getElementById('resultHeader').className = 'machineWinner';
+        document.getElementById('result-header').className = 'machineWinner';
     }
-    document.getElementById('resultHeader').innerHTML = resultHeader;
+
+    displayScores();
+    document.getElementById('result-header').innerHTML = resultHeader;
     document.getElementById('results').style.visibility = 'visible';
-    document.getElementById('resultHeader').style.visibility = 'visible';
-
-
-
+    document.getElementById('result-header').style.visibility = 'visible';
 }
